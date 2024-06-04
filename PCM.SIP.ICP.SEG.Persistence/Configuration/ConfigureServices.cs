eb5e0fa4b92fbfb3dd11b451d50b690e38d8b8ec
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-06-03 22:59:47 -0500

    Feat: update ConfigureServices class in Persistence.Repository project

--- PCM.SIP.ICP.SEG.Persistence/Configuration/ConfigureServices.cs
+++ PCM.SIP.ICP.SEG.Persistence/Configuration/ConfigureServices.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using PCM.SIP.ICP.SEG.Aplicacion.Interface;
+using PCM.SIP.ICP.SEG.Aplicacion.Interface.Persistence;
+using PCM.SIP.ICP.SEG.Persistence.Repository;
+using PCM.SIP.ICP.SEG.Persistence.Repository.Base;
 
 namespace PCM.SIP.ICP.SEG.Persistence.Configuration
 {
-    internal class ConfigureServices
+    public static class ConfigureServices
     {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPerfilRepository, PerfilRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
     }
 }
