ff3701747b6324291db042b3f9aac17a6a6c496a
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2024-06-12 21:47:36 -0500

    Feat: implement endpoints usuario

--- PCM.SIP.ICP.SEG.Api/Controllers/UsuarioController.cs
+++ PCM.SIP.ICP.SEG.Api/Controllers/UsuarioController.cs
@@ -1,84 +1,82 @@
 ﻿using AutoMapper;
+using System.Net;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PCM.SIP.ICP.SEG.Api.Filters;
 using PCM.SIP.ICP.SEG.Aplicacion.Dto;
-using PCM.SIP.ICP.SEG.Aplicacion.Features;
 using PCM.SIP.ICP.SEG.Aplicacion.Interface.Features;
-using PCM.SIP.ICP.SEG.Transversal.Common;
 using PCM.SIP.ICP.SEG.Transversal.Common.Generics;
-using System.Net;
+using PCM.SIP.ICP.SEG.Transversal.Common;
 
 namespace PCM.SIP.ICP.SEG.Api.Controllers
 {
     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class PerfilController : Controller
+    public class UsuarioController : Controller
     {
-        private readonly IPerfilApplication _perfilApplication;
+        private readonly IUsuarioApplication _usuarioApplication;
         private readonly IMapper _mapper;
 
-        public PerfilController(IPerfilApplication perfilApplication, IMapper mapper)
+        public UsuarioController(IUsuarioApplication usuarioApplication, IMapper mapper)
         {
-            _perfilApplication = perfilApplication;
+            _usuarioApplication = usuarioApplication;
             _mapper = mapper;
         }
 
         [HttpGet("GetById")]
         [ServiceFilter(typeof(AuthorizationRequestAttribute))]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(PcmResponse))]
-        public ActionResult<PcmResponse> GetById([FromQuery] PerfilIdRequest request)
+        public ActionResult<PcmResponse> GetById([FromQuery] UsuarioIdRequest request)
         {
             if (request == null)
                 return BadRequest();
 
-            return _perfilApplication.GetById(new Request<PerfilDto>() { entidad = _mapper.Map<PerfilDto>(request) });
+            return _usuarioApplication.GetById(new Request<UsuarioDto>() { entidad = _mapper.Map<UsuarioDto>(request) });
         }
 
         [HttpGet("GetList")]
         [ServiceFilter(typeof(AuthorizationRequestAttribute))]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(PcmResponse))]
-        public ActionResult<PcmResponse> GetList([FromQuery] PerfilFilterRequest request)
+        public ActionResult<PcmResponse> GetList([FromQuery] UsuarioFilterRequest request)
         {
             if (request == null)
                 return BadRequest();
 
-            return _perfilApplication.GetList(new Request<PerfilDto>() { entidad = _mapper.Map<PerfilDto>(request) });
+            return _usuarioApplication.GetList(new Request<UsuarioDto>() { entidad = _mapper.Map<UsuarioDto>(request) });
         }
 
         [HttpPost("Insert")]
         [ServiceFilter(typeof(AuthorizationRequestAttribute))]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(PcmResponse))]
-        public ActionResult<PcmResponse> Insert([FromBody] PerfilInsertRequest request)
+        public ActionResult<PcmResponse> Insert([FromBody] UsuarioInsertRequest request)
         {
             if (request == null)
                 return BadRequest();
 
-            return _perfilApplication.Insert(new Request<PerfilDto>() { entidad = _mapper.Map<PerfilDto>(request) });
+            return _usuarioApplication.Insert(new Request<UsuarioDto>() { entidad = _mapper.Map<UsuarioDto>(request) });
         }
 
         [HttpPut("Update")]
         [ServiceFilter(typeof(AuthorizationRequestAttribute))]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(PcmResponse))]
-        public ActionResult<PcmResponse> Update([FromBody] PerfilUpdateRequest request)
+        public ActionResult<PcmResponse> Update([FromBody] UsuarioUpdateRequest request)
         {
             if (request == null)
                 return BadRequest();
 
-            return _perfilApplication.Update(new Request<PerfilDto>() { entidad = _mapper.Map<PerfilDto>(request) });
+            return _usuarioApplication.Update(new Request<UsuarioDto>() { entidad = _mapper.Map<UsuarioDto>(request) });
         }
 
         [HttpDelete("Delete")]
         [ServiceFilter(typeof(AuthorizationRequestAttribute))]
         [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(PcmResponse))]
-        public ActionResult<PcmResponse> DeleteObject([FromBody] PerfilIdRequest request)
+        public ActionResult<PcmResponse> DeleteObject([FromBody] UsuarioIdRequest request)
         {
             if (request == null)
                 return BadRequest();
 
-            return _perfilApplication.Delete(new Request<PerfilDto>() { entidad = _mapper.Map<PerfilDto>(request) });
+            return _usuarioApplication.Delete(new Request<UsuarioDto>() { entidad = _mapper.Map<UsuarioDto>(request) });
         }
-
     }
 }
--- PCM.SIP.ICP.SEG.Aplicacion.Features/Features/UsuarioApplication.cs
+++ PCM.SIP.ICP.SEG.Aplicacion.Features/Features/UsuarioApplication.cs
@@ -1,35 +1,245 @@
-﻿using PCM.SIP.ICP.SEG.Aplicacion.Dto;
+﻿using AutoMapper;
+using System.Text.Json;
+using PCM.SIP.ICP.SEG.Aplicacion.Dto;
+using PCM.SIP.ICP.SEG.Aplicacion.Interface;
 using PCM.SIP.ICP.SEG.Aplicacion.Interface.Features;
+using PCM.SIP.ICP.SEG.Aplicacion.Validator;
+using PCM.SIP.ICP.SEG.Domain.Entities;
 using PCM.SIP.ICP.SEG.Transversal.Common;
+using PCM.SIP.ICP.SEG.Transversal.Common.Constants;
 using PCM.SIP.ICP.SEG.Transversal.Common.Generics;
+using PCM.SIP.ICP.Transversal.Util.Encryptions;
 
 namespace PCM.SIP.ICP.SEG.Aplicacion.Features
 {
     public class UsuarioApplication : IUsuarioApplication
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IAppLogger<UsuarioApplication> _logger;
+        private readonly IPcmSessionApplication _pcmSessionApplication;
+        private readonly UsuarioValidationManager _usuarioValidationManager;
+
+        public UsuarioApplication(
+            IUnitOfWork unitOfWork, 
+            IMapper mapper, 
+            IAppLogger<UsuarioApplication> logger, 
+            IPcmSessionApplication pcmSessionApplication, 
+            UsuarioValidationManager usuarioValidationManager)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+            _pcmSessionApplication = pcmSessionApplication;
+            _usuarioValidationManager = usuarioValidationManager;
+        }
+
         public PcmResponse Insert(Request<UsuarioDto> request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var validation = _usuarioValidationManager.Validate(_mapper.Map<UsuarioInsertRequest>(request.entidad));
+
+                if (!validation.IsValid)
+                {
+                    _logger.LogError(Validation.InvalidMessage);
+                    return ResponseUtil.BadRequest(validation.Errors != null ? validation.Errors : null, Validation.InvalidMessage);
+                }
+
+                var entidad = _mapper.Map<Usuario>(request.entidad);
+                entidad.usuario_reg = _pcmSessionApplication.UsuarioCache.username;
+
+                var result = _unitOfWork.Usuario.Insert(entidad);
+
+                if (result.Error)
+                {
+                    _logger.LogError(result.Message);
+                    return ResponseUtil.InternalError(message: result.Message);
+                }
+
+                _logger.LogInformation(result.Message ?? TransactionMessage.SaveSuccess);
+                return ResponseUtil.Created(message: TransactionMessage.SaveSuccess);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return ResponseUtil.InternalError(message: ex.Message);
+            }
         }
 
         public PcmResponse Update(Request<UsuarioDto> request)
         {
-            throw new NotImplementedException();
+            var validation = _usuarioValidationManager.Validate(_mapper.Map<UsuarioUpdateRequest>(request.entidad));
+
+            if (!validation.IsValid)
+            {
+                _logger.LogError(Validation.InvalidMessage);
+                return ResponseUtil.BadRequest(validation.Errors != null ? validation.Errors : null, Validation.InvalidMessage);
+            }
+
+            try
+            {
+                var entidad = _mapper.Map<Usuario>(request.entidad);
+
+                entidad.usuario_id = string.IsNullOrEmpty(request.entidad.SerialKey) ? 0 : Convert.ToInt32(CShrapEncryption.DecryptString(request.entidad.SerialKey, _pcmSessionApplication.UsuarioCache.authkey));
+                entidad.usuario_act = _pcmSessionApplication.UsuarioCache.username;
+
+                var result = _unitOfWork.Usuario.Update(entidad);
+
+                if (result.Error)
+                {
+                    _logger.LogError(result.Message);
+                    return ResponseUtil.InternalError(message: result.Message);
+                }
+
+                _logger.LogInformation(result.Message ?? TransactionMessage.UpdateSuccess);
+                return ResponseUtil.Created(message: TransactionMessage.UpdateSuccess);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return ResponseUtil.InternalError(message: ex.Message);
+            }
         }
 
         public PcmResponse Delete(Request<UsuarioDto> request)
         {
-            throw new NotImplementedException();
+            var validation = _usuarioValidationManager.Validate(_mapper.Map<UsuarioIdRequest>(request.entidad));
+
+            if (!validation.IsValid)
+            {
+                _logger.LogError(Validation.InvalidMessage);
+                return ResponseUtil.BadRequest(validation.Errors != null ? validation.Errors : null, Validation.InvalidMessage);
+            }
+
+            try
+            {
+                var entidad = _mapper.Map<Usuario>(request.entidad);
+
+                entidad.usuario_id = string.IsNullOrEmpty(request.entidad.SerialKey) ? 0 : Convert.ToInt32(CShrapEncryption.DecryptString(request.entidad.SerialKey, _pcmSessionApplication.UsuarioCache.authkey));
+                entidad.usuario_act = _pcmSessionApplication.UsuarioCache.username;
+
+                var result = _unitOfWork.Usuario.Delete(entidad);
+
+                if (result.Error)
+                {
+                    _logger.LogError(result.Message);
+                    return ResponseUtil.InternalError(message: result.Message);
+                }
+
+                _logger.LogInformation(TransactionMessage.DeleteSuccess);
+                return ResponseUtil.Created(message: result.Message ?? TransactionMessage.DeleteSuccess);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return ResponseUtil.InternalError(message: ex.Message);
+            }
         }
 
         public PcmResponse GetById(Request<UsuarioDto> request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var validation = _usuarioValidationManager.Validate(_mapper.Map<UsuarioIdRequest>(request.entidad));
+
+                if (!validation.IsValid)
+                {
+                    _logger.LogError(Validation.InvalidMessage);
+                    return ResponseUtil.BadRequest(validation.Errors != null ? validation.Errors : null, Validation.InvalidMessage);
+                }
+
+                var entidad = _mapper.Map<Usuario>(request.entidad);
+                entidad.usuario_id = string.IsNullOrEmpty(request.entidad.SerialKey) ? 0 : Convert.ToInt32(CShrapEncryption.DecryptString(request.entidad.SerialKey, _pcmSessionApplication.UsuarioCache.authkey));
+
+                var result = _unitOfWork.Usuario.GetById(entidad);
+
+                if (result.Error)
+                {
+                    _logger.LogError(result.Message);
+                    return ResponseUtil.InternalError(message: result.Message);
+                }
+
+                if (result.Data != null)
+                {
+                    entidad = new Usuario
+                    {
+                        SerialKey = string.IsNullOrEmpty(result.Data.usuario_id.ToString()) ? null : CShrapEncryption.EncryptString(result.Data.usuario_id.ToString(), _pcmSessionApplication.UsuarioCache.authkey),
+                        personakey = string.IsNullOrEmpty(result.Data.persona_id.ToString()) ? null : CShrapEncryption.EncryptString(result.Data.persona_id.ToString(), _pcmSessionApplication.UsuarioCache.authkey),
+                        username = result.Data.username,
+                        password = result.Data.password,
+                        numdocumento = result.Data.numdocumento,
+                        nombre_completo = result.Data.nombre_completo,
+                        interno = result.Data.interno,
+                        estado = result.Data.estado,
+                        usuario_reg = result.Data.usuario_reg,
+                        fecha_reg = result.Data.fecha_reg,
+                        usuario_act = result.Data.usuario_act,
+                        fecha_act = result.Data.fecha_act
+                    };
+                }
+
+                _logger.LogInformation(TransactionMessage.QuerySuccess);
+                return result.Data != null ? ResponseUtil.Ok(
+                    _mapper.Map<PerfilResponse>(_mapper.Map<PerfilDto>(entidad)), result.Message ?? TransactionMessage.QuerySuccess
+                    ) : ResponseUtil.NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return ResponseUtil.InternalError(message: ex.Message);
+            }
         }
 
         public PcmResponse GetList(Request<UsuarioDto> request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var entidad = _mapper.Map<Usuario>(request.entidad);
+
+                entidad.usuario_id = string.IsNullOrEmpty(request.entidad.SerialKey) ? 0 : Convert.ToInt32(CShrapEncryption.DecryptString(request.entidad.SerialKey, _pcmSessionApplication.UsuarioCache.authkey));
+
+                var result = _unitOfWork.Usuario.GetList(entidad);
+
+                if (result.Error)
+                {
+                    _logger.LogError(result.Message);
+                    return ResponseUtil.InternalError(message: result.Message);
+                }
+
+                List<Usuario> Lista = new List<Usuario>();
+
+                if (result.Data != null)
+                {
+                    foreach (var item in result.Data)
+                    {
+                        Lista.Add(new Usuario()
+                        {
+                            SerialKey = string.IsNullOrEmpty(item.usuario_id.ToString()) ? null : CShrapEncryption.EncryptString(item.usuario_id.ToString(), _pcmSessionApplication.UsuarioCache.authkey),
+                            personakey = string.IsNullOrEmpty(item.persona_id.ToString()) ? null : CShrapEncryption.EncryptString(item.persona_id.ToString(), _pcmSessionApplication.UsuarioCache.authkey),
+                            username = item.username,
+                            password = item.password,
+                            numdocumento = item.numdocumento,
+                            nombre_completo = item.nombre_completo,
+                            interno = item.interno,
+                            lista_perfiles = string.IsNullOrEmpty(item.json_perfiles) ? new List<Perfil>() : JsonSerializer.Deserialize<List<Perfil>>(item.json_perfiles),
+                            usuario_reg = item.usuario_reg,
+                            fecha_reg = item.fecha_reg
+                        });
+                    }
+                }
+
+                _logger.LogInformation(TransactionMessage.QuerySuccess);
+                return result != null ? ResponseUtil.Ok(
+                    _mapper.Map<List<PerfilResponse>>(_mapper.Map<List<PerfilDto>>(Lista)),
+                    result.Message ?? TransactionMessage.QuerySuccess
+                    ) : ResponseUtil.NoContent();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return ResponseUtil.InternalError(message: ex.Message);
+            }
         }
     }
 }
